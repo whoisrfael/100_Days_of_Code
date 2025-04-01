3089b5af8866faf1d633a322fa8bab2b2fe3447b
Rafael Fernandes <rfael.epr@gmail.com>
2025-04-01 00:17:51 -0300

    first commit

---
+++ dia_01/gerador_banda.py
@@ -0,0 +1,4 @@
+name = "Jack"
+
+print(name)
+print(len(name))
---
+++ dia_01/task.py
@@ -0,0 +1,10 @@
+#name = print("Hello " + input("What is your name? ")+ "!" )
+
+#name = "Jack"
+
+#print(len(name))
+
+username = input("What is your name?: ")
+lenght = len(username)
+
+print(lenght)
